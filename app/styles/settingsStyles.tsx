96024972b20e0453c65f8282e860c9a924cd68e0
ynestd <ynestd@gmail.com>
2026-04-04 00:47:15 -0400

    Changed the form for profile editting (added asteriks and required fields) + change the placement of the edit button and make the profile info collapsible

--- app/styles/settingsStyles.tsx
+++ app/styles/settingsStyles.tsx
@@ -1,351 +1,459 @@
 import { StyleSheet } from "react-native";
 import { ThemeType } from "../data/themeProvider";
 
-// const COLORS = {
-//   white: "#FFFFFF",
-//   black: "#111111",
-//   text: "#1F1F1F",
-//   subtext: "#4E4E4E",
-//   muted: "#6B7280",
-
-//   primary: "#56bab8",
-//   primaryDark: "#5a8c8b",
-//   pink: "#e7548b",
-//   lavender: "#9796b8",
-//   softPink: "#d6b1c3",
-
-//   border: "#E7E7EC",
-//   softBg: "#F9FAFB",
-//   tealTint: "#EEF9F8",
-//   pinkTint: "#FCEAF1",
-//   lavenderTint: "#F3F1FA",
-// };
-
 export const styles = (COLORS: ThemeType) =>
-  StyleSheet.create({
-    background: {
-      flex: 1,
-      backgroundColor: COLORS.white,
-      justifyContent: "space-between",
-    },
+    StyleSheet.create({
+        background: {
+            flex: 1,
+            backgroundColor: COLORS.white,
+            justifyContent: "space-between",
+        },
 
-    scrollableContent: {
-      paddingTop: 20,
-      paddingHorizontal: 20,
-      paddingBottom: 120,
-    },
+        scrollableContent: {
+            paddingTop: 24,
+            paddingHorizontal: 22,
+            paddingBottom: 130,
+        },
 
-    header: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      paddingHorizontal: 20,
-      paddingTop: 10,
-    },
+        header: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 22,
+            paddingTop: 12,
+            paddingBottom: 4,
+        },
 
-    title: {
-      fontSize: 27,
-      fontFamily: "Lexend_400Regular",
-      color: COLORS.black,
-      letterSpacing: -0.4,
-    },
+        title: {
+            fontSize: 28,
+            fontFamily: "Lexend_400Regular",
+            color: COLORS.black,
+            letterSpacing: -0.4,
+        },
 
-    tabContainer: {
-      flexDirection: "row",
-      backgroundColor: COLORS.softBg,
-      marginHorizontal: 20,
-      marginTop: 12,
-      marginBottom: 12,
-      borderRadius: 18,
-      padding: 4,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-    },
+        tabContainer: {
+            flexDirection: "row",
+            backgroundColor: COLORS.softBg,
+            marginHorizontal: 22,
+            marginTop: 14,
+            marginBottom: 6,
+            borderRadius: 18,
+            padding: 4,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+        },
 
-    tabButton: {
-      flex: 1,
-      paddingVertical: 12,
-      borderRadius: 14,
-      alignItems: "center",
-    },
+        tabButton: {
+            flex: 1,
+            paddingVertical: 12,
+            borderRadius: 14,
+            alignItems: "center",
+        },
 
-    activeTabButton: {
-      backgroundColor: COLORS.pinkTint,
-      borderWidth: 1,
-      borderColor: COLORS.softPink,
-    },
+        activeTabButton: {
+            backgroundColor: COLORS.pinkTint,
+            borderWidth: 1,
+            borderColor: COLORS.softPink,
+        },
 
-    tabButtonText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 14,
-      color: COLORS.subtext,
-    },
+        tabButtonText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 14,
+            color: COLORS.subtext,
+        },
 
-    activeTabButtonText: {
-      color: COLORS.black,
-    },
+        activeTabButtonText: {
+            color: COLORS.black,
+        },
 
-    profileCard: {
-      backgroundColor: COLORS.white,
-      borderRadius: 24,
-    },
+        profileCard: {
+            backgroundColor: COLORS.white,
+            borderRadius: 24,
+        },
 
-    profileHero: {
-      flexDirection: "row",
-      alignItems: "center",
-      backgroundColor: COLORS.tealTint,
-      borderRadius: 22,
-      padding: 16,
-      marginBottom: 18,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-    },
+        profileHero: {
+            flexDirection: "row",
+            alignItems: "center",
+            backgroundColor: COLORS.tealTint,
+            borderRadius: 24,
+            paddingVertical: 22,
+            paddingHorizontal: 18,
+            marginBottom: 26,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+        },
 
-    avatarCircle: {
-      width: 68,
-      height: 68,
-      borderRadius: 34,
-      backgroundColor: COLORS.primary,
-      alignItems: "center",
-      justifyContent: "center",
-      marginRight: 14,
-    },
+        avatarCircle: {
+            width: 72,
+            height: 72,
+            borderRadius: 36,
+            backgroundColor: COLORS.primary,
+            alignItems: "center",
+            justifyContent: "center",
+            marginRight: 16,
+        },
 
-    avatarText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 24,
-      color: COLORS.white,
-    },
+        avatarText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 24,
+            color: COLORS.white,
+        },
 
-    profileHeroText: {
-      flex: 1,
-    },
+        profileHeroText: {
+            flex: 1,
+        },
 
-    profileName: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 22,
-      color: COLORS.black,
-    },
+        profileName: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 22,
+            color: COLORS.black,
+            marginBottom: 2,
+        },
 
-    roleBadge: {
-      alignSelf: "flex-start",
-      backgroundColor: COLORS.white,
-      paddingHorizontal: 10,
-      paddingVertical: 5,
-      borderRadius: 999,
-      marginTop: 6,
-      marginBottom: 6,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-    },
+        roleBadge: {
+            alignSelf: "flex-start",
+            backgroundColor: COLORS.white,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+            borderRadius: 999,
+            marginTop: 6,
+            marginBottom: 8,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+        },
 
-    roleBadgeText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 12,
-      color: COLORS.primaryDark,
-    },
+        roleBadgeText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 12,
+            color: COLORS.primaryDark,
+        },
 
-    profileSubText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 13,
-      color: COLORS.subtext,
-    },
+        profileSubText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 13,
+            color: COLORS.subtext,
+            lineHeight: 18,
+        },
 
-    sectionBlock: {
-      marginTop: 8,
-      marginBottom: 16,
-    },
+        sectionBlock: {
+            marginTop: 2,
+            marginBottom: 26,
+        },
 
-    sectionTitle: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 16,
-      color: COLORS.black,
-      marginBottom: 10,
-    },
+        sectionHeader: {
+            marginBottom: 12,
+        },
 
-    infoCard: {
-      backgroundColor: COLORS.white,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-      borderRadius: 18,
-      padding: 14,
-    },
+        sectionTitle: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 17,
+            color: COLORS.black,
+            marginBottom: 6,
+        },
 
-    formCard: {
-      backgroundColor: COLORS.white,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-      borderRadius: 18,
-      padding: 14,
-    },
+        sectionCaption: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 12,
+            color: COLORS.muted,
+            lineHeight: 18,
+        },
 
-    infoRow: {
-      paddingVertical: 6,
-    },
+        collapsibleSectionWrap: {
+            marginBottom: 18,
+        },
 
-    infoDivider: {
-      height: 1,
-      backgroundColor: COLORS.border,
-      marginVertical: 8,
-    },
+        collapsiblePill: {
+            backgroundColor: COLORS.white,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 26,
+            minHeight: 82,
+            paddingHorizontal: 22,
+            paddingVertical: 18,
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-between",
 
-    profileLabel: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 13,
-      color: COLORS.muted,
-      marginBottom: 4,
-    },
+            shadowColor: "#000",
+            shadowOffset: { width: 0, height: 1 },
+            shadowOpacity: 0.04,
+            shadowRadius: 4,
+            elevation: 1,
+        },
 
-    profileValue: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 16,
-      color: COLORS.black,
-    },
+        collapsibleLeft: {
+            flex: 1,
+            paddingRight: 12,
+        },
 
-    profileInput: {
-      backgroundColor: COLORS.softBg,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-      borderRadius: 14,
-      paddingHorizontal: 14,
-      paddingVertical: 12,
-      marginBottom: 14,
-      fontFamily: "Lexend_400Regular",
-      fontSize: 15,
-      color: COLORS.black,
-    },
+        collapsibleTitle: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 17,
+            color: COLORS.black,
+        },
 
-    profileReadOnlyBox: {
-      backgroundColor: COLORS.softBg,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-      borderRadius: 14,
-      paddingHorizontal: 14,
-      paddingVertical: 12,
-      marginBottom: 14,
-    },
+        collapsibleRight: {
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "center",
+            minWidth: 34,
+        },
 
-    profileReadOnlyText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 15,
-      color: COLORS.subtext,
-    },
+        collapsibleChevron: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 28,
+            color: COLORS.primaryDark,
+            lineHeight: 28,
+            fontWeight: "700",
+        },
 
-    primaryProfileButtonFull: {
-      backgroundColor: COLORS.pink,
-      paddingVertical: 14,
-      borderRadius: 16,
-      alignItems: "center",
-      justifyContent: "center",
-      marginBottom: 18,
-      width: "100%",
-    },
+        collapsibleContentBox: {
+            backgroundColor: COLORS.white,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 24,
+            marginTop: 10,
+            paddingHorizontal: 22,
+            paddingVertical: 18,
+        },
 
-    primaryProfileButton: {
-      flex: 1,
-      backgroundColor: COLORS.pink,
-      paddingVertical: 14,
-      borderRadius: 16,
-      alignItems: "center",
-      justifyContent: "center",
-    },
+        infoItem: {
+            paddingBottom: 16,
+            marginBottom: 16,
+            borderBottomWidth: 1,
+            borderBottomColor: COLORS.border,
+        },
 
-    primaryProfileButtonText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 14,
-      color: COLORS.white,
-    },
+        infoItemLast: {
+            paddingBottom: 0,
+            marginBottom: 0,
+        },
 
-    secondaryProfileButton: {
-      flex: 1,
-      backgroundColor: COLORS.softBg,
-      paddingVertical: 14,
-      borderRadius: 16,
-      alignItems: "center",
-      justifyContent: "center",
-      borderWidth: 1,
-      borderColor: COLORS.border,
-    },
+        formCard: {
+            backgroundColor: COLORS.white,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 20,
+            paddingHorizontal: 16,
+            paddingVertical: 16,
+        },
 
-    secondaryProfileButtonText: {
-      fontFamily: "Lexend_400Regular",
-      fontSize: 14,
-      color: COLORS.primaryDark,
-    },
+        inputGroup: {
+            marginBottom: 18,
+        },
 
-    editActionsRow: {
-      flexDirection: "row",
-      columnGap: 10,
-      marginTop: 8,
-      marginBottom: 8,
-    },
+        profileLabel: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 13,
+            color: COLORS.muted,
+            marginBottom: 6,
+        },
 
-    settingsCard: {
-      backgroundColor: COLORS.white,
-      borderWidth: 1,
-      borderColor: COLORS.border,
-      borderRadius: 18,
-      overflow: "hidden",
-      marginBottom: 18,
-    },
+        requiredStar: {
+            color: COLORS.pink,
+        },
 
-    settingRow: {
-      flexDirection: "row",
-      justifyContent: "space-between",
-      alignItems: "center",
-      paddingHorizontal: 16,
-      paddingVertical: 16,
-    },
+        profileValue: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 16,
+            color: COLORS.black,
+            lineHeight: 22,
+        },
 
-    settingLabel: {
-      fontSize: 16,
-      fontFamily: "Lexend_400Regular",
-      color: COLORS.black,
-    },
+        profileInput: {
+            backgroundColor: COLORS.softBg,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 16,
+            paddingHorizontal: 14,
+            paddingVertical: 13,
+            fontFamily: "Lexend_400Regular",
+            fontSize: 15,
+            color: COLORS.black,
+        },
 
-    settingsDivider: {
-      height: 1,
-      backgroundColor: COLORS.border,
-      marginHorizontal: 16,
-    },
+        profileInputError: {
+            borderColor: COLORS.pink,
+        },
 
-    dangerButton: {
-      backgroundColor: COLORS.pinkTint,
-      borderRadius: 16,
-      paddingVertical: 14,
-      alignItems: "center",
-      justifyContent: "center",
-      marginBottom: 10,
-      borderWidth: 1,
-      borderColor: "#F4C9D9",
-    },
+        errorText: {
+            marginTop: 8,
+            fontFamily: "Lexend_400Regular",
+            fontSize: 12,
+            color: COLORS.pink,
+            lineHeight: 16,
+        },
 
-    dangerButtonText: {
-      fontSize: 16,
-      fontFamily: "Lexend_400Regular",
-      color: COLORS.pink,
-    },
+        profileReadOnlyBox: {
+            backgroundColor: COLORS.softBg,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 16,
+            paddingHorizontal: 14,
+            paddingVertical: 13,
+        },
 
-    dangerButtonSecondary: {
-      backgroundColor: COLORS.lavenderTint,
-      borderRadius: 16,
-      paddingVertical: 14,
-      alignItems: "center",
-      justifyContent: "center",
-      borderWidth: 1,
-      borderColor: "#DDDCEF",
-    },
+        profileReadOnlyText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 15,
+            color: COLORS.subtext,
+        },
 
-    dangerButtonSecondaryText: {
-      fontSize: 16,
-      fontFamily: "Lexend_400Regular",
-      color: COLORS.lavender,
-    },
+        lockedLabelRow: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            marginBottom: 6,
+        },
 
-    preferencesModalScreen: {
-      flex: 1,
-      backgroundColor: COLORS.white,
-      paddingHorizontal: 16,
-      paddingTop: 8,
-    },
-  });
+        lockedBadge: {
+            backgroundColor: COLORS.lavenderTint,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 999,
+            paddingHorizontal: 10,
+            paddingVertical: 4,
+        },
+
+        lockedBadgeText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 11,
+            color: COLORS.lavender,
+        },
+
+        disabledFieldBox: {
+            backgroundColor: COLORS.softBg,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 16,
+            paddingHorizontal: 14,
+            paddingVertical: 13,
+            opacity: 0.75,
+        },
+
+        disabledFieldText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 15,
+            color: COLORS.muted,
+        },
+
+        readOnlyHint: {
+            marginTop: 8,
+            fontFamily: "Lexend_400Regular",
+            fontSize: 12,
+            color: COLORS.muted,
+            lineHeight: 17,
+        },
+
+        primaryProfileButtonFull: {
+            backgroundColor: COLORS.pink,
+            paddingVertical: 15,
+            borderRadius: 18,
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: 24,
+            width: "100%",
+        },
+
+        primaryProfileButton: {
+            flex: 1,
+            backgroundColor: COLORS.pink,
+            paddingVertical: 15,
+            borderRadius: 18,
+            alignItems: "center",
+            justifyContent: "center",
+        },
+
+        primaryProfileButtonText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 14,
+            color: COLORS.white,
+        },
+
+        secondaryProfileButton: {
+            flex: 1,
+            backgroundColor: COLORS.softBg,
+            paddingVertical: 15,
+            borderRadius: 18,
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 1,
+            borderColor: COLORS.border,
+        },
+
+        secondaryProfileButtonText: {
+            fontFamily: "Lexend_400Regular",
+            fontSize: 14,
+            color: COLORS.primaryDark,
+        },
+
+        editActionsRow: {
+            flexDirection: "row",
+            columnGap: 12,
+            marginTop: 4,
+            marginBottom: 12,
+        },
+
+        settingsCard: {
+            backgroundColor: COLORS.white,
+            borderWidth: 1,
+            borderColor: COLORS.border,
+            borderRadius: 20,
+            overflow: "hidden",
+            marginBottom: 20,
+        },
+
+        settingRow: {
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+            paddingHorizontal: 18,
+            paddingVertical: 18,
+        },
+
+        settingLabel: {
+            fontSize: 16,
+            fontFamily: "Lexend_400Regular",
+            color: COLORS.black,
+        },
+
+        dangerButton: {
+            backgroundColor: COLORS.pinkTint,
+            borderRadius: 18,
+            paddingVertical: 15,
+            alignItems: "center",
+            justifyContent: "center",
+            marginBottom: 12,
+            borderWidth: 1,
+            borderColor: "#F4C9D9",
+        },
+
+        dangerButtonText: {
+            fontSize: 16,
+            fontFamily: "Lexend_400Regular",
+            color: COLORS.pink,
+        },
+
+        dangerButtonSecondary: {
+            backgroundColor: COLORS.lavenderTint,
+            borderRadius: 18,
+            paddingVertical: 15,
+            alignItems: "center",
+            justifyContent: "center",
+            borderWidth: 1,
+            borderColor: "#DDDCEF",
+        },
+
+        dangerButtonSecondaryText: {
+            fontSize: 16,
+            fontFamily: "Lexend_400Regular",
+            color: COLORS.lavender,
+        },
+
+        preferencesModalScreen: {
+            flex: 1,
+            backgroundColor: COLORS.white,
+            paddingHorizontal: 16,
+            paddingTop: 8,
+        },
+    });
